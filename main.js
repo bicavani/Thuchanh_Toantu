fda09f6d7c2b1dfbc1664c3a14d31a0568888829
Namlongcomputer <hoandm91@gmail.com>
2019-03-26 15:53:36 +0700

    version1

---
+++ main.js
@@ -0,0 +1,8 @@
+let inputWidth;
+let inputHeight;
+inputWidth = prompt("Nhap gia tri Width = ", '');
+inputHeight  = prompt("Nhap gia tri Height = ", '');
+let width = parseInt(inputWidth);
+let height = parseInt(inputHeight);
+let area = width*height;
+document.write("The area is: " + area);
